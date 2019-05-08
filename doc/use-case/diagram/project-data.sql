4532351bf1e3369cfb47a75d339ed66d6dd0204c
gwanghosong <gwanghosong91@gmail.com>
2019-05-09 01:30:53 +0900

    DB모델링 수정
    auto-increment 적용, 필요없는 컬럼 삭제, 날짜값 default 설정, sql예시 정리 및 추가

--- doc/use-case/diagram/project-data.sql
+++ doc/use-case/diagram/project-data.sql
@@ -9,64 +9,26 @@ insert into boardtype(boardtype_id, boardtype_name)
 values(4, '매치보드게시판');
 insert into boardtype(boardtype_id, boardtype_name)
 values(5, '고객센터');
-insert into boardtype(boardtype_id, boardtype_name)se
+insert into boardtype(boardtype_id, boardtype_name)
 values(6, '개인정보');
 insert into boardtype(boardtype_id, boardtype_name)
 values(7, '나의팀정보');
 insert into boardtype(boardtype_id, boardtype_name)
 values(8, '질문게시판');
 
-insert into mbr(
-mbr_id,
-user_id,
-pwd,
-name,
-bth_dt,
-gndr,
-tel,
-email,
-bas_addr,
-det_addr,
-prof_phot,
-s_intd,
-pid_scop,
-login_type,
-stat,
-cdt,
-lcd,
-wthd_wth,
-wthd_res,
-wthd_dt,
-mgr
-)
-values(1,'testId','tt','김민철','1995-5-5','남','010-1111-1111','test@com','비트캠프','강남','aaaa','안녕','1','face','정상','2020-2-2','2020-2-2',false,'그냥','2020-2-2',true);
+--회원데이터
+insert into mbr(mbr_id,user_id,pwd,name,bth_dt,gndr,tel,bas_addr,det_addr,login_type)
+values(1,'testId','1111','김민철','1995-5-5','남','010-1111-1111','비트캠프','강남','face');
+insert into mbr(mbr_id,user_id,pwd,name,bth_dt,gndr,tel,bas_addr,det_addr,login_type)
+values(2,'test2','1111','test2','1991-01-01','남','010-1234-5678','서울','강남','kakao');
+insert into mbr(mbr_id,user_id,pwd,name,bth_dt,gndr,tel,bas_addr,det_addr,login_type)
+values(3,'test3','1111','test3','1981-01-01','여','010-3334-5555','부산','해운대','google');
 
-insert into post(
-post_id,
-mbr_id,
-boardtype_id,
-titl,
-cont,
-vw_cnt,
-cdt,
-mdt,
-stat,
-del_res
-)
-values(1,1,1,'test','testtest','1','2020-2-2','2020-2-2','정상','그냥');
-
-insert into post(
-post_id,
-mbr_id,
-boardtype_id,
-titl,
-cont,
-vw_cnt,
-cdt,
-mdt,
-stat,
-del_res
-)
-values(2,1,2,'test','testtest','1','2020-2-2','2020-2-2','정상','그냥');
-
+--게시판데이터
+insert into post(post_id,mbr_id,boardtype_id,titl,cont)
+values(1,1,1,'test1','testtest1');
+insert into post(post_id,mbr_id,boardtype_id,titl,cont)
+values(2,1,1,'test2','testtest2');
+insert into post(post_id,mbr_id,boardtype_id,titl,cont)
+values(3,1,1,'test3','testtest3');
 
