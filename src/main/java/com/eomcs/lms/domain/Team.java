d71f84b280d3948b38d18ee9d59733b75839de63
kmincheol <mincheol.kmc@gmail.com>
2019-05-09 10:58:46 +0900

    team_domain

---
+++ src/main/java/com/eomcs/lms/domain/Team.java
@@ -0,0 +1,127 @@
+package com.eomcs.lms.domain;
+import java.io.Serializable;
+import java.sql.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class Team implements Cloneable, Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private int teamId;
+  private String name;
+  private String teamInfo;
+  private String area;
+  private int ageId;
+  private int teamTypeId;
+  private int teamLevelId;
+  private int sportsId;
+  private String teamEmblemPhoto;
+  private boolean teamUniform;
+  private String teamUniformPhoto;
+  private Date teamCreateDate;
+  private Date teamDeleteDate;
+  private String teamDeleteReason;
+  private Date teamInfoModifieDate;
+
+  @Override
+  public String toString() {
+    return "Team [teamId=" + teamId + ", name=" + name + ", teamInfo=" + teamInfo + ", area=" + area
+        + ", ageId=" + ageId + ", teamTypeId=" + teamTypeId + ", teamLevelId=" + teamLevelId
+        + ", sportsId=" + sportsId + ", teamEmblemPhoto=" + teamEmblemPhoto + ", teamUniform="
+        + teamUniform + ", teamUniformPhoto=" + teamUniformPhoto + ", teamCreateDate="
+        + teamCreateDate + ", teamDeleteDate=" + teamDeleteDate + ", teamDeleteReason="
+        + teamDeleteReason + ", teamInfoModifieDate=" + teamInfoModifieDate + "]";
+  }
+  
+  public int getTeamId() {
+    return teamId;
+  }
+  public void setTeamId(int teamId) {
+    this.teamId = teamId;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getTeamInfo() {
+    return teamInfo;
+  }
+  public void setTeamInfo(String teamInfo) {
+    this.teamInfo = teamInfo;
+  }
+  public String getArea() {
+    return area;
+  }
+  public void setArea(String area) {
+    this.area = area;
+  }
+  public int getAgeId() {
+    return ageId;
+  }
+  public void setAgeId(int ageId) {
+    this.ageId = ageId;
+  }
+  public int getTeamTypeId() {
+    return teamTypeId;
+  }
+  public void setTeamTypeId(int teamTypeId) {
+    this.teamTypeId = teamTypeId;
+  }
+  public int getTeamLevelId() {
+    return teamLevelId;
+  }
+  public void setTeamLevelId(int teamLevelId) {
+    this.teamLevelId = teamLevelId;
+  }
+  public int getSportsId() {
+    return sportsId;
+  }
+  public void setSportsId(int sportsId) {
+    this.sportsId = sportsId;
+  }
+  public String getTeamEmblemPhoto() {
+    return teamEmblemPhoto;
+  }
+  public void setTeamEmblemPhoto(String teamEmblemPhoto) {
+    this.teamEmblemPhoto = teamEmblemPhoto;
+  }
+  public boolean isTeamUniform() {
+    return teamUniform;
+  }
+  public void setTeamUniform(boolean teamUniform) {
+    this.teamUniform = teamUniform;
+  }
+  public String getTeamUniformPhoto() {
+    return teamUniformPhoto;
+  }
+  public void setTeamUniformPhoto(String teamUniformPhoto) {
+    this.teamUniformPhoto = teamUniformPhoto;
+  }
+  public Date getTeamCreateDate() {
+    return teamCreateDate;
+  }
+  public void setTeamCreateDate(Date teamCreateDate) {
+    this.teamCreateDate = teamCreateDate;
+  }
+  public Date getTeamDeleteDate() {
+    return teamDeleteDate;
+  }
+  public void setTeamDeleteDate(Date teamDeleteDate) {
+    this.teamDeleteDate = teamDeleteDate;
+  }
+  public String getTeamDeleteReason() {
+    return teamDeleteReason;
+  }
+  public void setTeamDeleteReason(String teamDeleteReason) {
+    this.teamDeleteReason = teamDeleteReason;
+  }
+  public Date getTeamInfoModifieDate() {
+    return teamInfoModifieDate;
+  }
+  public void setTeamInfoModifieDate(Date teamInfoModifieDate) {
+    this.teamInfoModifieDate = teamInfoModifieDate;
+  }
+  
+ 
+}
