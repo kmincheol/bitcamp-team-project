68eb970fe8b6e864976d996412890e2495b5e6f9
Gwangho Song <gwanghosong91@gmail.com>
2019-05-29 14:36:00 +0900

    에러페이지수정

--- src/main/java/com/eomcs/lms/web/exceptionHandler/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/eomcs/lms/web/exceptionHandler/GlobalDefaultExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.eomcs.lms.web.exceptionHandler;
 
-
 import javax.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-
 
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
