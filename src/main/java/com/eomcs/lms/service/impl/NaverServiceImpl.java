fdb64f3494257bf32a0b025b44257ff8cc8069ef
Gwangho Song <gwanghosong91@gmail.com>
2019-06-12 15:46:39 +0900

    .

--- src/main/java/com/eomcs/lms/service/impl/NaverServiceImpl.java
+++ src/main/java/com/eomcs/lms/service/impl/NaverServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
